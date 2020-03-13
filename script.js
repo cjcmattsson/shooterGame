465e0a03973ed6b4736dd24e64554bed782e5065
Christopher Mattsson <cjcmattsson@gmail.com>
2020-03-13 16:45:03 +0100

    new stuff

--- script.js
+++ script.js
@@ -3,20 +3,27 @@ let board = document.querySelector('.board');
 let hits = 0;
 let counter = document.querySelector('.counter');
 
-function createTarget() {
-  let target = document.createElement('div');
-  target.setAttribute('class', 'target');
-  target.setAttribute('onclick', 'hit(event)');
-  board.appendChild(target);
+function random(min, max) {
+  return min + Math.random() * (max - min);
 }
 
-function hit(e) {
-  console.log(e.target);
-  e.target.parentNode.removeChild(e.target);
+function createTarget() {
+  let target = document.createElement('div');
+  let hitZone = document.createElement('div');
+  target.setAttribute('class', 'target');
+  hitZone.setAttribute('class', 'hitZone');
+  hitZone.appendChild(target);
+  hitZone.onclick = hit;
+  hitZone.style.top = random((board.offsetHeight - 0), (board.offsetHeight - 450)) + "px";
+  board.appendChild(hitZone);
+}
+
+function hit() {
+  this.remove();
   hits ++;
   counter.innerHTML = hits;
 }
 
 setInterval(function(){
   createTarget();
-}, 3000)
+}, 1000)
