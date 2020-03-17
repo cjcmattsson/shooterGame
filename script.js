1c0b4167a57bd3766bf6ebd372d813e2306e8e6f
Christopher Mattsson <cjcmattsson@gmail.com>
2020-03-17 16:45:55 +0100

    Loads of styling and added sprites

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let board = document.querySelector('.board');
 let hits = 0;
 let counter = document.querySelector('.counter');
 
+let startButton = document.querySelector('.starter .start-message button');
+let startScreen = document.querySelector('.starter');
+
+let direction = ['left', 'right'];
+let object = ['dragon', 'bottle'];
+
 function random(min, max) {
   return min + Math.random() * (max - min);
 }
@@ -10,11 +16,13 @@ function random(min, max) {
 function createTarget() {
   let target = document.createElement('div');
   let hitZone = document.createElement('div');
-  target.setAttribute('class', 'target');
+  target.classList.add('target');
+  target.classList.add(object[Math.floor(Math.random() * object.length)]);
   hitZone.setAttribute('class', 'hitZone');
+  hitZone.classList.add(direction[Math.floor(Math.random() * direction.length)])
   hitZone.appendChild(target);
   hitZone.onclick = hit;
-  hitZone.style.top = random((board.offsetHeight - 0), (board.offsetHeight - 450)) + "px";
+  hitZone.style.top = random((board.offsetHeight - 100), (board.offsetHeight - 450)) + "px";
   board.appendChild(hitZone);
 }
 
@@ -24,6 +32,11 @@ function hit() {
   counter.innerHTML = hits;
 }
 
-setInterval(function(){
-  createTarget();
-}, 1000)
+function startGame() {
+  startScreen.classList.add('gameStarted');
+  setInterval(function(){
+    createTarget();
+  }, 1000)
+}
+
+startButton.addEventListener('click', startGame);
